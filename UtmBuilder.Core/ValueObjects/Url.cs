06f102076f2491b7916ff03b50c3c3ca0cae8f93
DELCINEY LEMOS OLIVEIRA <delciney07@gmail.com>
2024-04-22 14:19:45 -0300

    Resultado da implementação do curso

---
+++ UtmBuilder.Core/ValueObjects/Url.cs
@@ -0,0 +1,23 @@
+﻿
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.ValueObjects
+{
+    public class Url : ValueObject
+    {
+        /// <summary>
+        ///  Address of URL (Website link)
+        /// </summary>
+        public string Address { get; }
+
+        /// <summary>
+        ///  Create a new URL 
+        /// </summary>
+        /// <param name="address">Address of URL (Website link)</param>
+        public Url(string address)
+        {
+            Address = address;
+            InvalidUrlException.ThrowIfInvalid(address);
+        }
+    }
+}
